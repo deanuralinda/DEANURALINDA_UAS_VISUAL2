6d12e7b7d90bd89355f08ad34d48c86d15ed0c0f
deanuralinda <deadna3@gmail.com>
2023-06-23 23:15:39 +0700

    upload pertama

---
+++ Unit1.pas
@@ -0,0 +1,212 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Grids, DBGrids, DB, ZAbstractRODataset,
+  ZAbstractDataset, ZDataset, ZAbstractConnection, ZConnection, frxClass,
+  frxDBSet;
+
+type
+  TForm1 = class(TForm)
+    con1: TZConnection;
+    zqry1: TZQuery;
+    ds1: TDataSource;
+    dbgrd1: TDBGrid;
+    lbl1: TLabel;
+    lbl2: TLabel;
+    lbl3: TLabel;
+    lbl4: TLabel;
+    lbl5: TLabel;
+    edt1: TEdit;
+    edt2: TEdit;
+    edt3: TEdit;
+    edt5: TEdit;
+    btn1: TButton;
+    btn2: TButton;
+    btn3: TButton;
+    btn4: TButton;
+    btn5: TButton;
+    edt4: TEdit;
+    btn6: TButton;
+    frxdbdtst1: TfrxDBDataset;
+    frxrprt1: TfrxReport;
+    procedure btn1Click(Sender: TObject);
+    procedure bersih;
+    procedure posisiawal;
+    procedure FormShow(Sender: TObject);
+    procedure btn2Click(Sender: TObject);
+    procedure btn4Click(Sender: TObject);
+    procedure btn5Click(Sender: TObject);
+    procedure dbgrd1CellClick(Column: TColumn);
+    procedure btn3Click(Sender: TObject);
+    procedure btn6Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  id: String;
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.btn1Click(Sender: TObject);
+begin
+bersih;
+btn1.Enabled:= false;
+btn2.Enabled:= True;
+btn3.Enabled:= False;
+btn4.Enabled:= False;
+btn5.Enabled:= True;
+edt1.Enabled:= True;
+edt2.Enabled:= True;
+edt3.Enabled:= True;
+edt4.Enabled:= True;
+edt5.Enabled:= True
+end;
+
+procedure TForm1.posisiawal;
+begin
+bersih;
+btn1.Enabled:= True;
+btn2.Enabled:= False;
+btn3.Enabled:= False;
+btn4.Enabled:= False;
+btn5.Enabled:= False;
+edt1.Enabled:= False;
+edt2.Enabled:= False;
+edt3.Enabled:= False;
+edt4.Enabled:= False;
+edt5.Enabled:= False;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+posisiawal;
+end;
+
+procedure TForm1.bersih;
+begin
+edt1.Clear;
+edt2.Clear;
+edt3.Clear;
+edt4.Clear;
+edt5.Clear;
+end;
+
+procedure TForm1.btn2Click(Sender: TObject);
+begin
+if edt1.Text ='' then
+begin
+ShowMessage('ID HUBUNGAN TIDAK BOLEH KOSONG!');
+end else
+if edt2.Text ='' then
+begin
+ShowMessage('ID SISWA TIDAK BOLEH KOSONG!');
+end else
+if edt3.Text ='' then
+begin
+ShowMessage('ID ORTU TIDAK BOLEH KOSONG!');
+end else
+if edt4.Text ='' then
+begin
+ShowMessage('STATUS HUBUNGAN TIDAK BOLEH KOSONG!');
+end else
+if edt5.Text ='' then
+begin
+ShowMessage('KETERANGAN TIDAK BOLEH KOSONG!');
+end else
+begin
+zqry1.SQL.Clear; //simpan
+zqry1.SQL.Add('insert into tabel_hubungan values("'+edt1.Text+'","'+edt2.Text+'","'+edt3.Text+'","'+edt4.Text+'","'+edt5.Text+'")');
+zqry1.ExecSQL ;
+
+zqry1.SQL.Clear;
+zqry1.SQL.Add('select * from tabel_hubungan');
+zqry1.Open;
+ShowMessage('DATA BARHASIL DISIMPAN!');
+posisiawal;
+end;
+end;
+
+procedure TForm1.btn4Click(Sender: TObject);
+begin
+if MessageDlg('APAKAH YAKIN MENGHAPUS DATA INI?',mtWarning,[mbYes,mbNo],0)= mryes then
+begin
+zqry1.SQL.Clear;
+zqry1.SQL.Add(' delete from tabel_hubungan where id_hubungan="'+id+'"');
+zqry1. ExecSQL;
+zqry1.SQL.Clear;
+zqry1.SQL.Add('select * from tabel_hubungan');
+zqry1.Open;
+ShowMessage('DATA BERHASIL DIHAPUS');
+posisiawal;
+end else
+begin
+ ShowMessage('DATA BATAL DIHAPUS');
+ posisiawal;
+end;
+end;
+
+procedure TForm1.btn5Click(Sender: TObject);
+begin
+posisiawal;
+end;
+
+procedure TForm1.dbgrd1CellClick(Column: TColumn);
+begin
+edt1.Text:= zqry1.Fields[0].AsString;
+edt2.Text:= zqry1.Fields[1].AsString;
+edt3.Text:= zqry1.Fields[2].AsString;
+edt4.Text:= zqry1.Fields[3].AsString;
+edt5.Text:= zqry1.Fields[4].AsString;
+edt1.Enabled:= True;
+edt2.Enabled:= True;
+edt3.Enabled:= True;
+edt4.Enabled:= True;
+edt5.Enabled:= True;
+
+btn1.Enabled:= false;
+btn2.Enabled:= True;
+btn3.Enabled:= True;
+btn4.Enabled:= True;
+btn5.Enabled:= True;
+end;
+
+procedure TForm1.btn3Click(Sender: TObject);
+begin
+if (edt1.Text= '')or (edt2.Text ='')or(edt3.Text= '')or (edt4.Text ='')or (edt5.Text ='') then
+begin
+  ShowMessage('INPUTAN WAJIB DIISI!');
+end else
+if edt1.Text = zqry1.Fields[1].AsString then
+begin
+ ShowMessage('DATA TIDAK ADA PERUBAHAN');
+ posisiawal;
+end else
+if edt1.Text = zqry1.Fields[0].AsString then
+begin
+ ShowMessage('DATA BERHASIL DIUPDATE!');
+zqry1.SQL.Clear;
+zqry1.SQL.Add('Update tabel_hubungan set id_hubungan= "'+edt1.Text+'",id_ortu="'+edt2.Text+'" where id_hubungan="'+id+'"');
+zqry1. ExecSQL;
+
+zqry1.SQL.Clear;
+zqry1.SQL.Add('select * from tabel_hubungan');
+zqry1.Open;
+
+posisiawal;
+end;
+end;
+
+procedure TForm1.btn6Click(Sender: TObject);
+begin
+frxrprt1.ShowReport();
+end;
+
+end.
